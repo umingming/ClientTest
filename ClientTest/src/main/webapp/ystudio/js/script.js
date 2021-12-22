a264a404536b531d5302cb93326a168a9721d4ea
umingming <u_0112@naver.com>
2021-12-22 22:24:08 +0900

    Add slide function

--- ClientTest/src/main/webapp/ystudio/js/script.js
+++ ClientTest/src/main/webapp/ystudio/js/script.js
@@ -4,12 +4,49 @@ $(document).on('click', 'a[href="#"]', function(e){
     e.preventDefault();
 });
 
+//gnb메뉴
 $(function(){
     $('header .menuOpen').on('click', function(){
         $('.gnb').addClass('on');
     });
     $('.gnb .close, section').on('click', function(){
         $('.gnb').removeClass('on');
-    })
+    });
 });
 
+//fixHeader
+var scrollTop = 0;
+scrollTop = $(document).scrollTop();
+fixHeader();
+
+$(window).on('scroll resize', function () {
+    scrollTop = $(document).scrollTop();
+    fixHeader();
+});
+
+
+function fixHeader() {
+    if(scrollTop > 150) {
+        $('header').addClass('on');
+    } else {
+        $('header').removeClass('on');
+    }
+}
+
+
+$(function() {
+    $('.animate').scrolla({
+        mobile: false,
+        once: false
+    });
+});
+
+
+
+
+//상단으로 부드럽게
+$(function() {
+    $('.goTop').on('click', function(){
+        $('html, body').animate({scrollTop: 0}, 400);
+    });
+});
