d00590530ed12889ebb2f243ba9c4851fee149eb
umingming <u_0112@naver.com>
2021-12-27 23:54:06 +0900

    Update menu

--- ClientTest/src/main/webapp/clone/daebang/js/script.js
+++ ClientTest/src/main/webapp/clone/daebang/js/script.js
@@ -9,3 +9,25 @@ $(function(){
         $('.gnb .inner').removeClass('on')
     });
 });
+
+//fixheader
+
+var scrollFix = 0;
+scrollFix = $(document).scrollTop();
+fixHeader();
+
+
+//윈도우창 조절
+$(window).on('scroll resize', function(){
+    scrollFix = $(document).scrollTop();
+    fixHeader();
+})
+
+//고정 헤더 함수 
+function fixHeader() {
+    if(scrollFix > 150) {
+        $('header').addClass('on');
+    } else {
+        $('header').removeClass('on');
+    }
+}
